9929fd74cd8c6e3d8fbd83d87f105d23842a1c0c
David Brooks <dave@bcs.co.nz>
2018-10-23 14:27:14 +1300

    Improve assignment of cells to edges.

--- script/edgeClustering.js
+++ script/edgeClustering.js
@@ -25,13 +25,16 @@ limitations under the License.
 //import * as math from '../thirdparty/math.js';
 
 import {SVG_NS} from './svgElements.js';
+import {setAttributes} from './utils.js';
 
 //==============================================================================
 
 class Cell
 {
-    constructor()
+    constructor(topLeft, size)
     {
+        this._topLeft = [topLeft[0], topLeft[1]];
+        this._size = [size[0], size[1]];
         this._nodes = [];
         this._edges = [];
     }
@@ -45,7 +48,34 @@ class Cell
     add_edge(edge)
     //============
     {
+        // Store normalised edge angle -- calculate once and pass as parameter?
         this._edges.push(edge);
+    }
+
+    generateSvg()
+    //===========
+    {
+        const fill = (this._nodes.length && this._edges.length) ? "red"
+                   : (this._nodes.length) ? "green"
+                   : (this._edges.length) ? "blue"
+                   : "none";
+        const svgNode = document.createElementNS(SVG_NS, 'rect');
+        setAttributes(svgNode, { x: this._topLeft[0], y: this._topLeft[1],
+                                 width: this._size[0], height: this._size[1],
+                                 fill: fill, opacity: 0.7
+                               });
+        return svgNode;
+    }
+}
+
+//==============================================================================
+
+class Edge
+{
+    constructor(nodes)
+    {
+        this.start = nodes[0].coordinates;
+        this.end = nodes[1].coordinates;
     }
 }
 
@@ -67,10 +97,15 @@ class Grid
         this._gridStepSize = [(boundingBox[1][0] - boundingBox[0][0])/gridSteps,
                               (boundingBox[1][1] - boundingBox[0][1])/gridSteps]
         this._cellMatrix = math.zeros(this._gridSteps, this._gridSteps);
+        const startPos = [this._topLeft[0] - 0.5*this._gridStepSize[0],
+                          this._topLeft[1] - 0.5*this._gridStepSize[1]];
         for (let i = 0; i < this._gridSteps; i += 1) {
             for (let j = 0; j < this._gridSteps; j += 1) {
-                this._cellMatrix.set([i, j], new Cell());
+                this._cellMatrix.set([i, j], new Cell(startPos, this._gridStepSize));
+                startPos[1] += this._gridStepSize[1];
             }
+            startPos[0] += this._gridStepSize[0];
+            startPos[1] = this._topLeft[1] - 0.5*this._gridStepSize[1];
         }
     }
 
@@ -96,16 +131,24 @@ class Grid
     addEdgeLineLow(start,startCoords, end,endCoords, edge)
     //====================================================
     {
-        const slope = (end.y - start.y)/(end.x - start.x);
+        const slope = (end.y - start.y)*this._gridStepSize[0]/((end.x - start.x)*this._gridStepSize[1]);
         const yIncrement = (slope >= 0) ? 1 : -1;
         let error = (0.5 + (start.y - this._topLeft[1])/this._gridStepSize[1]) - startCoords[1];
+        if (slope < 0) error = 1 - error;
         let y = startCoords[1];
-        for (let x = startCoords[0]; x <= endCoords[0]; x += 1) {
-            this._cellMatrix.get([x, y]).add_edge(edge);
+        //console.log('from', start.asArray(), startCoords, '  to', end.asArray(), endCoords, '  slope', slope, '  error', error);
+        for (let x = startCoords[0]; x < endCoords[0]; x += 1) {
+            if (x > startCoords[0]) {
+                this._cellMatrix.get([x, y]).add_edge(edge);
+            }
             error += Math.abs(slope);
-            if (error > 1.0) {
+            //console.log(x, y, error);
+            while (error > 1.0) {
                 error -= 1;
                 y += yIncrement;
+                if (x > startCoords[0] && error > 1) {
+                    this._cellMatrix.get([x, y]).add_edge(edge);
+                }
             }
         }
     }
@@ -115,16 +158,24 @@ class Grid
     addEdgeLineHigh(start,startCoords, end,endCoords, edge)
     //=====================================================
     {
-        const slope = (end.x - start.x)/(end.y - start.y);
+        const slope = (end.x - start.x)*this._gridStepSize[1]/((end.y - start.y)*this._gridStepSize[0]);
         const xIncrement = (slope >= 0) ? 1 : -1;
         let error = (0.5 + (start.x - this._topLeft[0])/this._gridStepSize[0]) - startCoords[0];
+        if (slope < 0) error = 1 - error;
         let x = startCoords[0];
-        for (let y = startCoords[1]; y <= endCoords[1]; y += 1) {
-            this._cellMatrix.get([x, y]).add_edge(edge);
+        //console.log('from', start.asArray(), startCoords, '  to', end.asArray(), endCoords, '  slope', slope, '  error', error);
+        for (let y = startCoords[1]; y < endCoords[1]; y += 1) {
+            if (y > startCoords[1]) {
+                this._cellMatrix.get([x, y]).add_edge(edge);
+            }
             error += Math.abs(slope);
-            if (error > 1.0) {
+            //console.log(x, y, error);
+            while (error > 1.0) {
                 error -= 1;
                 x += xIncrement;
+                if (y > startCoords[1] && error > 1) {
+                    this._cellMatrix.get([x, y]).add_edge(edge);
+                }
             }
         }
     }
@@ -132,34 +183,39 @@ class Grid
     add_edge(edge)
     //============
     {
-        //connection.path is a linestring
-
-        //path.coordinates[0]
-        //path.coordinates.slice(-1)[0]
-
-        const start = edge[0].coordinates;
-        const startCoords = this.gridCoords(start.asArray());
-        const end = edge[1].coordinates;
-        const endCoords = this.gridCoords(end.asArray());
+        const startCoords = this.gridCoords(edge.start.asArray());
+        const endCoords = this.gridCoords(edge.end.asArray());
         if        (startCoords[0] === endCoords[0]) {
-            for (let y = startCoords[1]; y <= endCoords[1]; y += 1) {
-                this._cellMatrix.get([startCoords[0], y]).add_edge(edge);
+            if (endCoords[1] > startCoords[1]) {
+                for (let y = startCoords[1] + 1; y < endCoords[1]; y += 1) {
+                    this._cellMatrix.get([startCoords[0], y]).add_edge(edge);
+                }
+            } else {
+                for (let y = startCoords[1] - 1; y > endCoords[1]; y -= 1) {
+                    this._cellMatrix.get([startCoords[0], y]).add_edge(edge);
+                }
             }
         } else if (startCoords[1] === endCoords[1]) {
-            for (let x = startCoords[0]; x <= endCoords[0]; x += 1) {
-                this._cellMatrix.get([x, startCoords[1]]).add_edge(edge);
-            }
-        } else if (Math.abs(end.y - start.y) < Math.abs(end.x - start.x)) {
-            if (start.x > end.x) {
-                this.addEdgeLineLow(end,endCoords, start,startCoords, edge);
+            if (endCoords[0] > startCoords[0]) {
+                for (let x = startCoords[0] + 1; x < endCoords[0]; x += 1) {
+                    this._cellMatrix.get([x, startCoords[1]]).add_edge(edge);
+                }
             } else {
-                this.addEdgeLineLow(start,startCoords, end,endCoords, edge);
+                for (let x = startCoords[0] - 1; x > endCoords[0]; x -= 1) {
+                    this._cellMatrix.get([x, startCoords[1]]).add_edge(edge);
+                }
+            }
+        } else if (Math.abs(edge.end.y - edge.start.y) < Math.abs(edge.end.x - edge.start.x)) {
+            if (edge.start.x > edge.end.x) {
+                this.addEdgeLineLow(edge.end,endCoords, edge.start,startCoords, edge);
+            } else {
+                this.addEdgeLineLow(edge.start,startCoords, edge.end,endCoords, edge);
             }
         } else {
-            if (start.y > end.y) {
-                this.addEdgeLineHigh(end,endCoords, start,startCoords, edge);
+            if (edge.start.y > edge.end.y) {
+                this.addEdgeLineHigh(edge.end,endCoords, edge.start,startCoords, edge);
             } else {
-                this.addEdgeLineHigh(start,startCoords, end,endCoords, edge);
+                this.addEdgeLineHigh(edge.start,startCoords, edge.end,endCoords, edge);
             }
         }
 
@@ -172,15 +228,15 @@ class Grid
         */
     }
 
-    svgNode()
-    //=======
+    generateSvg()
+    //===========
     {
         const GRID_COLOUR = "red";
         const GRID_OPACITY = 0.7;
 
         const gridId = `cluster_grid`;
         const gridStrokeWidth = 2.0;
-        const gridStrokeDash = `${2*gridStrokeWidth} ${2*gridStrokeWidth}`;
+        const gridStrokeDash = `${gridStrokeWidth} ${gridStrokeWidth}`;
         const gridFraction = 1.0/this._gridSteps;
 
         const gridSvg = `<g class="grid_">
@@ -199,8 +255,15 @@ class Grid
       width="${this._gridSteps*this._gridStepSize[0]}" height="${this._gridSteps*this._gridStepSize[1]}"/>
 </g>`;
         const parser = new DOMParser();
-        const svgNode = parser.parseFromString(gridSvg, "application/xml");
-        return svgNode.documentElement;
+        const svgDocument = parser.parseFromString(gridSvg, "application/xml");
+        const svgNode = svgDocument.documentElement;
+
+        for (let i = 0; i < this._gridSteps; i += 1) {
+            for (let j = 0; j < this._gridSteps; j += 1) {
+                svgNode.appendChild(this._cellMatrix.get([i, j]).generateSvg());
+            }
+        }
+        return svgNode;
     }
 }
 
@@ -209,7 +272,7 @@ class Grid
 
 export class EdgeClusterer
 {
-    constructor(gridSteps=4, directionThreshold=15, alpha=0.7, beta=0.3)
+    constructor(gridSteps=15, directionThreshold=15, alpha=0.7, beta=0.3)
     {
         this._gridSteps = gridSteps;
         this._directionThreshold = directionThreshold;
@@ -225,9 +288,9 @@ export class EdgeClusterer
         for (let node of graph.nodes()) {
             const [x, y] = node.coordinates.asArray();
             if (x < xMin) xMin = x;
-            else if (x > xMax) xMax = x;
+            if (x > xMax) xMax = x;
             if (y < yMin) yMin = y;
-            else if (y > yMax) yMax = y;
+            if (y > yMax) yMax = y;
         }
         if (xMin === xMax) xMax = xMin + 1;
         if (yMin === yMax) yMax = yMin + 1;
@@ -244,9 +307,9 @@ export class EdgeClusterer
                 grid.add_node(node);
             }
             for (let edge of graph.edges()) {
-                grid.add_edge(edge);
+                grid.add_edge(new Edge(edge));
             }
-            return grid.svgNode();
+            return grid.generateSvg();
         } else {
             return document.createElementNS(SVG_NS, 'g');
         }
