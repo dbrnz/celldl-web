761c534a9eec5b564bdefdc55f647d49511cb695
David Brooks <dave@bcs.co.nz>
2018-10-25 14:35:19 +1300

    Refactor initialisation code to remove button event handlers from TextEditor class.

--- script/main.js
+++ script/main.js
@@ -24,72 +24,248 @@ limitations under the License.
 
 import {CellDiagram} from './cellDiagram.js';
 import {DiagramEditor} from './diagramEditor.js';
+import {Palette} from './palette.js';
 import {StyleSheet} from './stylesheet.js';
+import {TextEditor} from './textEditor.js';
+
+import {saveAs} from '../thirdparty/FileSaver.js';
 
 //==============================================================================
 
-export function displayDiagram(textEditor, svgContainerNode, palette)
+class CellDlFile
 {
-    return new Promise((resolve, reject) => {
-        // Remove any existing content from our SVG container
+    constructor(svgContainerId, paletteId)
+    {
+        this._editor = new TextEditor();
+        // Check editor.isClean() before loading a new file or closing window
+        // and then editor.markClean()
 
-        for (let child of svgContainerNode.children) {
-            child.remove();
+        this._loadedFile = '';
+        this._svgContainerNode = document.getElementById(svgContainerId);
+        this._palette = new Palette(document.getElementById(paletteId));
+        this.diagram = null;
+
+        // Start with an empty diagram
+        this._editor.setValue(`<cell-diagram>
+    <style>
+        cell-diagram {
+            width:  500;  /* SVG 'pixels' */
+            height: 500;  /* SVG 'pixels' */
         }
+    </style>
+</cell-diagram>`);
+        this._editor.clearSelection();
+        this.previewSvg();
+    }
 
-        const cellDlText = textEditor.getValue();
-        if (cellDlText === '') {
-            reject("No CellDL to display");
-        }
+    upLoadedFileAsText(file)
+    //======================
+    {
+        const reader = new FileReader();
 
-        const domParser = new DOMParser();
-        const xmlDocument = domParser.parseFromString(cellDlText, "application/xml");
-        document.body.style.cursor = 'wait';
+        return new Promise((resolve, reject) => {
+            reader.onerror = () => {
+                reader.abort();
+                reject(reader.error);
+            }
+            reader.onload = () => {
+                resolve(reader.result);
+            }
 
-        const cellDiagram = new CellDiagram('diagram', textEditor);
-        try {
-          cellDiagram.parseDocument(xmlDocument)
-            .then(() => {
-                try {
-                    cellDiagram.layout();  // Pass width/height to use as defaults...
+            this._loadedFile = file.name;
+            reader.readAsText(file);
+        });
+    }
 
-                    const svgDiagram = cellDiagram.generateSvg();
-
-                    // Wait until all MathJax text has been rendered
-
-                    cellDiagram.svgFactory.promises().then(() => {
-                        // Show the SVG diagram
-                        // Note: If we use `appendChild` then `url()` links in the SVG
-                        //       document are not resolved
-                        svgContainerNode.insertAdjacentHTML('afterbegin', svgDiagram.outerHTML);
-
-                        // Reset busy wheel
-                        document.body.style.cursor = 'default';
-
-                        const svgNode = svgContainerNode.children[0];
-
-                        const diagramEditor = new DiagramEditor(cellDiagram, palette);
-
-                        const grid = diagramEditor.gridSvg();
-                        if (grid !== null) {
-                            svgNode.insertAdjacentHTML('beforeend', grid.outerHTML);
-                         }
-
-                        diagramEditor.svgLoaded(svgNode);
-
-                        resolve(cellDiagram);
-                    })
-                } catch (error) {
-                    reject(error);
-                }
-            }, error => { throw error; })
-            .catch(error => {
-                reject(error);
+    load(fileList)
+    //============
+    {
+    // Also want to be able to load remote file by URL
+        for (let file of fileList) {
+            this.upLoadedFileAsText(file).then(text => {
+                this._editor.setValue(text, 0);
+                this._editor.clearSelection();
+                this.previewSvg();
             });
-        } catch (error) {
-            reject(error);
+            break;
         }
-    });
+    }
+
+    displayDiagram()
+    {
+        return new Promise((resolve, reject) => {
+            // Remove any existing content from our SVG container
+
+            for (let child of this._svgContainerNode.children) {
+                child.remove();
+            }
+
+            const cellDlText = this._editor.getValue();
+            if (cellDlText === '') {
+                reject("No CellDL to display");
+            }
+
+            const domParser = new DOMParser();
+            const xmlDocument = domParser.parseFromString(cellDlText, "application/xml");
+            document.body.style.cursor = 'wait';
+
+            const cellDiagram = new CellDiagram('diagram', this._editor);
+            try {
+              cellDiagram.parseDocument(xmlDocument)
+                .then(() => {
+                    try {
+                        cellDiagram.layout();  // Pass width/height to use as defaults...
+
+                        const svgDiagram = cellDiagram.generateSvg();
+
+                        // Wait until all MathJax text has been rendered
+
+                        cellDiagram.svgFactory.promises().then(() => {
+                            // Show the SVG diagram
+                            // Note: If we use `appendChild` then `url()` links in the SVG
+                            //       document are not resolved
+                            this._svgContainerNode.insertAdjacentHTML('afterbegin', svgDiagram.outerHTML);
+
+                            // Reset busy wheel
+                            document.body.style.cursor = 'default';
+
+                            const svgNode = this._svgContainerNode.children[0];
+
+                            const diagramEditor = new DiagramEditor(cellDiagram, this._palette);
+
+                            const grid = diagramEditor.gridSvg();
+                            if (grid !== null) {
+                                svgNode.insertAdjacentHTML('beforeend', grid.outerHTML);
+                             }
+
+                            diagramEditor.svgLoaded(svgNode);
+
+                            resolve(cellDiagram);
+                        })
+                    } catch (error) {
+                        reject(error);
+                    }
+                }, error => { throw error; })
+                .catch(error => {
+                    reject(error);
+                });
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+
+    save()
+    //====
+    {
+        const text = this._editor.getValue();
+        const blob = new Blob([text], { type: "application/xml;charset=utf-8" });
+        saveAs(blob, this._loadedFile);
+    }
+
+    previewSvg()
+    //==========
+    {
+        this.displayDiagram().then(
+            (diagram) => { this._diagram = diagram; },
+            (error) => {
+                document.body.style.cursor = 'default';
+                console.trace(error);
+                alert(error);
+                this._diagram = null;
+            });
+    }
+
+    saveSvg(svgDiagram)
+    //=================
+    {
+        const svg = svgDiagram.outerHTML;
+        const blob = new Blob([svg], { type: "image/svg+xml" });
+        const svgFileName = `${this._loadedFile.split('.')[0]}.svg`
+        saveAs(blob, svgFileName);
+    }
+
+    exportSvg()
+    //=========
+    {
+        // Render the diagram without selected elements and grid
+
+        this._diagram.svgFactory.resetPromises();
+
+        const svgDiagram = this._diagram.generateSvg();
+
+        // Wait until all MathJax text has been rendered
+
+        if (this._diagram.svgFactory.promises().length) {
+            Promise.all(this._diagram.svgFactory.promises()).then(() => {
+                this.saveSvg(svgDiagram);
+            });
+        } else {
+            this.saveSvg(svgDiagram);
+        }
+    }
+
+    connectionMatrix(connectionMatrixId)
+    //==================================
+    {
+        const connections = this._diagram.bondGraph.adjacencyMatrix();
+
+        const html = ['<div>'];
+        // NB. close must be a button we listen for as `href` refreshes page...
+        html.push(' <a href="" class="connection-popup-close" title="Close">X</a>');
+        html.push(' <div class="connection-table">');
+        let r = 1;
+        for (let row of connections) {
+            const cls = (r == 1) ? ' label'
+                       : (r % 2) ? ' odd'
+                       : '';
+            html.push(`  <div class="connection-row${cls}">`);
+            let c = 1;
+            for (let cell of row) {
+                const cls_id = (r == 1 && c == 1) ? '" id="cell00'
+                             : (r == 1 || c == 1) ? ' label'
+                             : (c % 2) ? ' odd'
+                             : '';
+                const value = (cell != 0) ? cell : '';
+                html.push(`   <span class="connection-cell${cls_id}">${value}</span>`);
+                c += 1;
+            }
+            html.push('  </div>');
+            r += 1;
+        }
+        html.push(' </div>');
+        html.push('</div>');
+        const matrixHtml = html.join('\n');
+
+        const matrixElement = document.getElementById(connectionMatrixId);
+        matrixElement.innerHTML = matrixHtml;
+
+        matrixElement.style.opacity = 1;
+        matrixElement.style['pointer-events'] = 'auto';
+    /*
+        close
+            opacity:0;
+            pointer-events: none;
+            clear innerHtml
+    */
+    }
 }
 
 //==============================================================================
+
+export function main(svgContainerId, paletteId)
+{
+    const cellDlFile = new CellDlFile(svgContainerId, paletteId);
+
+    // Expose editor's functions to HTML elements
+
+    window.loadCellDl = (fileList) => cellDlFile.load(fileList);
+    window.saveCellDl = () => cellDlFile.save();
+    window.previewSvg = () => cellDlFile.previewSvg();
+    window.exportSvg = () => cellDlFile.exportSvg();
+    window.connectionMatrix = () => cellDlFile.connectionMatrix();
+
+}
+
+//==============================================================================
+
--- script/textEditor.js
+++ script/textEditor.js
@@ -22,15 +22,6 @@ limitations under the License.
 
 //==============================================================================
 
-import {displayDiagram} from './main.js';
-import {Palette} from './palette.js';
-
-//==============================================================================
-
-import {saveAs} from '../thirdparty/FileSaver.js';
-
-//==============================================================================
-
 export class TextEditor
 {
     constructor()
@@ -42,14 +33,6 @@ export class TextEditor
             hScrollBarAlwaysVisible: true
         });
         this.editor.session.setMode("ace/mode/xml");
-
-        // Check editor.isClean() before loading a new file or closing window
-        // and then editor.markClean()
-
-        this.diagram = null;
-        this.loadedFile = '';
-        this.svgContainerNode = document.getElementById('cell-diagram');
-        this.palette = new Palette(document.getElementById('palette'));
     }
 
     clearSelection()
@@ -68,121 +51,6 @@ export class TextEditor
     //=================
     {
         this.editor.setValue(text, pos);
-    }
-
-    upLoadedFileAsText(file)
-    //======================
-    {
-        const reader = new FileReader();
-
-        return new Promise((resolve, reject) => {
-            reader.onerror = () => {
-                reader.abort();
-                reject(reader.error);
-            }
-            reader.onload = () => {
-                resolve(reader.result);
-            }
-
-            this.loadedFile = file.name;
-            reader.readAsText(file);
-        });
-    }
-
-    loadCellDl(files)
-    //===============
-    {
-        for (let file of files) {
-            this.upLoadedFileAsText(file).then(text => {
-                this.editor.setValue(text, 0);
-                this.editor.clearSelection();
-                this.previewSvg();
-            });
-            break;
-        }
-    }
-
-    saveCellDl()
-    //==========
-    {
-        const text = this.editor.getValue();
-        const blob = new Blob([text], { type: "application/xml;charset=utf-8" });
-        saveAs(blob, this.loadedFile);
-    }
-
-    previewSvg()
-    //==========
-    {
-        displayDiagram(this, this.svgContainerNode, this.palette).then(
-            (diagram) => { this.diagram = diagram; },
-            (error) => {
-                document.body.style.cursor = 'default';
-                console.trace(error);
-                alert(error);
-                this.diagram = null;
-            });
-    }
-
-    saveSvg(svgDiagram)
-    //=================
-    {
-        const svg = svgDiagram.outerHTML;
-        const blob = new Blob([svg], { type: "image/svg+xml" });
-        const svgFileName = `${this.loadedFile.split('.')[0]}.svg`
-        saveAs(blob, svgFileName);
-    }
-
-    exportSvg()
-    //=========
-    {
-        // Render the diagram without selected elements and grid
-
-        this.diagram.svgFactory.resetPromises();
-
-        const svgDiagram = this.diagram.generateSvg();
-
-        // Wait until all MathJax text has been rendered
-
-        if (this.diagram.svgFactory.promises().length) {
-            Promise.all(this.diagram.svgFactory.promises()).then(() => {
-                this.saveSvg(svgDiagram);
-            });
-        } else {
-            this.saveSvg(svgDiagram);
-        }
-    }
-
-    connectionMatrix()
-    //================
-    {
-        const connections = this.diagram.bondGraph.connectionMatrix();
-
-        const html = ['<div>'];
-        html.push(' <a href="" class="connection-popup-close" title="Close">X</a>');
-        html.push(' <div class="connection-table">');
-        let r = 1;
-        for (let row of connections) {
-            const odd = (r % 2) ? ' odd' : '';
-            html.push(`  <div class="connection-row${odd}">`);
-            let c = 1;
-            for (let cell of row) {
-                const odd = (c % 2) ? ' odd' : '';
-                const value = (cell != 0) ? cell : '';
-                html.push(`   <span class="connection-cell${odd}">${value}</span>`);
-                c += 1;
-            }
-            html.push('  </div>');
-            r += 1;
-        }
-        html.push(' </div>');
-        html.push('</div>');
-        const matrixHtml = html.join('\n');
-
-        const matrixElement = document.getElementById('connection-matrix');
-        matrixElement.innerHTML = matrixHtml;
-
-        matrixElement.style.opacity = 1;
-        matrixElement.style['pointer-events'] = 'auto';
     }
 }
 
