21e870e13749995f84df581854a1e28e7dcc12ab
David Brooks <dave@bcs.co.nz>
2018-11-25 10:17:17 +1300

    Actually use version 3.0.1 of `cytoscape-canvas`.

--- thirdparty/cytoscape-canvas_v3.0.1.js
+++ thirdparty/cytoscape-canvas_v3.0.1.js
@@ -1,13 +1,13 @@
-(function() {
+(function () {
 	// registers the extension on a cytoscape lib ref
-	const register = function(cytoscape) {
+	const register = function (cytoscape) {
 		if (!cytoscape) {
 			return;
 		}
 
-		const cyCanvas = function(args) {
+		const cyCanvas = function (args) {
 			const cy = this;
-			const container = cy.container().children[0];  // <div> element holding canvases
+			const container = cy.container();
 
 			const canvas = document.createElement("canvas");
 
@@ -44,10 +44,7 @@
 				resize();
 			});
 
-			canvas.setAttribute(
-				"style",
-				`position:absolute; z-index:${options.zIndex};`,
-			);
+			canvas.setAttribute("style", `position:absolute; top:0; left:0; z-index:${options.zIndex};`);
 
 			resize();
 
@@ -94,20 +91,17 @@
 		cytoscape("core", "cyCanvas", cyCanvas);
 	};
 
-	if (typeof module !== "undefined" && module.exports) {
-		// expose as a commonjs module
-		module.exports = function(cytoscape) {
+	if (typeof module !== "undefined" && module.exports) { // expose as a commonjs module
+		module.exports = function (cytoscape) {
 			register(cytoscape);
 		};
 	}
 
-	if (typeof define !== "undefined" && define.amd) {
-		// expose as an amd/requirejs module
+	if (typeof define !== "undefined" && define.amd) { // expose as an amd/requirejs module
 		define("cytoscape-canvas", () => register);
 	}
 
-	if (typeof cytoscape !== "undefined") {
-		// expose to global cytoscape (i.e. window.cytoscape)
+	if (typeof cytoscape !== "undefined") { // expose to global cytoscape (i.e. window.cytoscape)
 		register(cytoscape);
 	}
-})();
+}());
